78509b7b88dcc6935671a4fc49bd94d26a661a14
carlosjunior <carlosjunior20313@gmail.com>
2023-03-11 12:24:36 -0300

    third commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+const initializeMainModule = require('./handlers/main');
+
+
+const bot2 = initializeMainModule();
+const {bot} = require('./bot')
+
+
+
+const startCommand = require('./commands/start');
+bot.onText(/\/start/, startCommand);
