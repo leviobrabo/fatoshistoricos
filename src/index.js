24983fb469e48fb72387c3ab4225a3891943fe62
leviobrabo <carlosjunior20313@gmail.com>
2023-03-23 19:56:06 -0300

    update

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const initializeMainModule = require("./handlers/main");
 const bot2 = initializeMainModule();
 const { bot } = require("./bot");
 
-const startCommand = require("./commands/start");
+const { startCommand } = require("./commands/start");
 const { histimag } = require("./commands/histimag");
 const { helpCommand } = require("./commands/help");
 
-bot.onText(/^\/start/, startCommand);
+bot.onText(/^\/start/, async (message) => {
+    await startCommand(bot, message);
+});
 
 bot.onText(/^\/fotoshist/, async (message) => {
     await histimag(bot, message);
