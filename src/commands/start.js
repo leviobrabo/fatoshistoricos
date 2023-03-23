ce84829fd1a805abefd000e4a8e79a192c9cc65a
leviobrabo <carlosjunior20313@gmail.com>
2023-03-23 20:37:05 -0300

    UPDATE GERAL

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,77 +1,55 @@
-function startCommand(bot, message) {
-    if (message.chat.type === "private") {
+const { bot } = require("../bot");
+
+module.exports = async (msg) => {
+    if (msg.chat.type === "private") {
+        const imageURL = "https://i.imgur.com/MzZuN3G.jpeg";
+
+        const firstName = message.from.first_name;
+        const message = `Olá, *${firstName}*, eu sou *Fatos Históricos!* \n\nSou um bot que envia diáriamente mensagem com fatos históricos acontecido no dia do envio da mensagem. \n\nAdicione-me em seu grupo.\n\n📦*Meu código-fonte:* [GitHub](https://github.com/leviobrabo/climatologiabot)`;
+
+        const buttons = [
+            [
+                {
+                    text: "Adicione-me em seu grupo",
+                    url: "https://t.me/climatologiabot?startgroup=true",
+                },
+            ],
+            [
+                { text: "👾 Canal", url: "https://t.me/lbrabo" },
+                { text: "🪪 Dono", url: "https://t.me/Kylorensbot" },
+            ],
+            [{ text: "Fazer uma doação 💰", callback_data: "/donate" }],
+        ];
+
+        await bot.sendPhoto(msg.chat.id, imageURL, {
+            caption: message,
+            disable_web_page_preview: true,
+            parse_mode: "Markdown",
+            reply_markup: {
+                inline_keyboard: buttons,
+            },
+        });
+    }
+};
+
+bot.on("callback_query", async (callbackQuery) => {
+    if (callbackQuery.message.chat.type !== "private") {
         return;
     }
+    const msg = callbackQuery.message;
+    const data = callbackQuery.data;
 
-    const firstName = message.from.first_name;
+    if (data === "/donate") {
+        const usuario = msg.from.first_name;
+        const chavePix = "32dc79d2-2868-4ef0-a277-2c10725341d4";
+        const banco = "Picpay";
+        const nome = "Luzia";
 
-    const welcomeMessage = `Olá, *${firstName}*!\n\nEu sou *Fatos Históricos!* \n\nSou um bot que envia diáriamente mensagem com fatos históricos acontecido no dia do envio da mensagem. \n\nAdicione-me em seu grupo.\n\n📦*Meu código-fonte:* [GitHub](https://github.com/leviobrabo/climatologiabot)`;
-    const options = {
-        parse_mode: "Markdown",
-        disable_web_page_preview: true,
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Adicione-me em seu grupo",
-                        url: "https://t.me/fatoshistbot?startgroup=true",
-                    },
-                ],
-                [
-                    { text: "👾 Canal", url: "https://t.me/lbrabo" },
-                    { text: "🪪 Dono", url: "https://t.me/Kylorensbot" },
-                ],
-                [{ text: "Fazer uma doação 💰", callback_data: "donate" }],
-            ],
-        },
-    };
-    bot.on("callback_query", (callbackQuery) => {
-        if (callbackQuery.message.chat.type !== "private") {
-            return;
-        }
-        const chatId = callbackQuery.message.chat.id;
-        const messageId = callbackQuery.message.message_id;
+        const resposta = `Olá, ${usuario}! \n\nContribua com qualquer valor para ajudar a manter o servidor do bot online e com mais recursos! Sua ajuda é fundamental para mantermos o bot funcionando de forma eficiente e com novas funcionalidades. \n\nPara fazer uma doação, utilize a chave PIX a seguir: \nPix: \`\`\`${chavePix}\`\`\` \nBanco: ${banco}\nNome: ${nome}\n\nObrigado pela sua contribuição! 🙌"`;
 
-        if (callbackQuery.data === "donate") {
-            const usuario = message.from.first_name;
-            const chavePix = "32dc79d2-2868-4ef0-a277-2c10725341d4";
-            const banco = "Picpay";
-            const nome = "Luzia";
-
-            const resposta = `Olá, ${usuario}! \n\nContribua com qualquer valor para ajudar a manter o servidor do bot online e com mais recursos! Sua ajuda é fundamental para mantermos o bot funcionando de forma eficiente e com novas funcionalidades. \n\nPara fazer uma doação, utilize a chave PIX a seguir: \nPix: \`${chavePix}\` \nBanco: ${banco}\nNome: ${nome}\n\nObrigado pela sua contribuição! 🙌"`;
-
-            bot.editMessageText(resposta, {
-                chat_id: chatId,
-                message_id: messageId,
-                parse_mode: "Markdown",
-                reply_markup: {
-                    inline_keyboard: [
-                        [
-                            {
-                                text: "Suporte",
-                                url: "t.me/kylorensbot",
-                            },
-                            {
-                                text: "Voltar",
-                                callback_data: "back_to_start",
-                            },
-                        ],
-                    ],
-                },
-            });
-        } else if (callbackQuery.data === "back_to_start") {
-            bot.editMessageText(welcomeMessage, {
-                parse_mode: "Markdown",
-                chat_id: chatId,
-                message_id: messageId,
-                disable_web_page_preview: true,
-                reply_markup: options.reply_markup,
-            });
-        }
-    });
-    bot.sendMessage(message.chat.id, welcomeMessage, options);
-}
-
-module.exports = {
-    startCommand,
-};
+        bot.sendMessage(msg.chat.id, resposta, {
+            reply_to_message_id: msg.message_id,
+            parse_mode: "Markdown",
+        });
+    }
+});
--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ const initializeMainModule = require("./handlers/main");
 const bot2 = initializeMainModule();
 const { bot } = require("./bot");
 
-const { startCommand } = require("./commands/start");
+const startCommand = require("./commands/start");
 const { histimag } = require("./commands/histimag");
 const { helpCommand } = require("./commands/help");
 
-bot.onText(/^\/start/, (message) => {
-    startCommand(bot, message);
-});
+bot.onText(/^\/start/, startCommand);
 
 bot.onText(/^\/fotoshist/, async (message) => {
     await histimag(bot, message);
