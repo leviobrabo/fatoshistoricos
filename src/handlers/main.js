0d2a9ddfd376769090eee88e32df1a1362e6fb52
leviobrabo <carlosjunior20313@gmail.com>
2023-05-07 14:30:09 -0300

    mudado o horario (teste)

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -567,7 +567,7 @@ async function sendHistoricalEventsChannel(channelIdEn) {
 }
 
 const channelEnJob = new CronJob(
-    "0 6 * * *",
+    "31 14 * * *",
     function () {
         sendHistoricalEventsChannel(channelIdEn);
         console.log(`Mensagem enviada com sucesso para o canal ${channelIdEn}`);
