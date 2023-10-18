201acf8cb15e34589370b3e73eaf4b0b0560bdc5
Levi <115052769+leviobrabo@users.noreply.github.com>
2023-10-18 16:30:22 -0300

    Update main.js

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -1019,7 +1019,6 @@ bot.onText(/\/unban/, async (message) => {
     const chatId = message.text.split(" ")[1];
 
     if (message.chat.type !== "private") {
-        await bot.sendMessage(
             console.log("Por favor, envie este comando em um chat privado com o bot.")
         );
         return;
@@ -1082,7 +1081,6 @@ bot.onText(/\/banned/, async (message) => {
     const userId = message.from.id;
 
     if (message.chat.type !== "private") {
-        await bot.sendMessage(
             console.log("Por favor, envie este comando em um chat privado com o bot.")
         );
         return;
