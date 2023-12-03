98cda9337818aa17a7aa1778e9b0d923735920b7
leviobrabo <carlosjunior20313@gmail.com>
2023-12-03 14:29:33 -0300

    git pull

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -1944,18 +1944,13 @@ sendBotOnlineMessage();
 
 async function alterarEsquema() {
     try {
-        // Encontrar todos os documentos na coleção UserModel
-        const usuarios = await UserModel.find({});
-
-        // Iterar sobre cada usuário e adicionar os campos se não estiverem presentes
-        for (const usuario of usuarios) {
-            if (!usuario.hits || !usuario.questions || !usuario.progress) {
-                usuario.hits = 0;
-                usuario.questions = 0;
-                usuario.progress = 0;
-                await usuario.save(); // Salvar as alterações no documento
+        await ChatModel.updateMany(
+            {},
+            {
+                $rename: { "chatId": "chat_id", "chatName": "chat_name", "isBlocked": "blocked" },
+                $set: { "question": false }
             }
-        }
+        );
 
         console.log('Alterações realizadas com sucesso.');
     } catch (error) {
@@ -1964,4 +1959,3 @@ async function alterarEsquema() {
 }
 
 alterarEsquema();
-
